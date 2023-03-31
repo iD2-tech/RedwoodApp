9cf29ef96f72947dd20ca9ababd6dd939bedbfe1
DavyHyun <dhyun1124@gmail.com>
2023-03-31 14:18:27 -0700

    Bible Api Sample Working

--- screens/In/CreatePost.js
+++ screens/In/CreatePost.js
@@ -1,10 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native'
+import React, { useState, useEffect } from 'react';
+
+
 
 const CreatePost = () => {
+
+  const [verses, setVerses] = useState('');
+  const [book, setBook] = useState('');
+  const [chapter, setChapter] = useState('');
+  const [verse, setVerse] = useState('');
+
+  // useEffect(() => {
+  //   setVerses('');
+  //   setBook('');
+  //   setChapter('');
+  //   setVerse('');
+  // }, []);
+
+  const fetchVerses = async () => {
+    try {
+      const response = await fetch(`https://bible-api.com/${book}${chapter}:${verse}`);
+      const data = await response.json();
+      setVerses(data.text);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View style={{backgroundColor: 'white', height: '100%', justifyContent: 'center', alignItems: 'center'}}>
-      <Text>CreatePost</Text>
+               <TextInput 
+                    placeholder="book"
+                    value={book}
+                    onChangeText={text => {setBook(text)}}
+                    style={styles.input}
+                    
+                />
+                
+                <TextInput 
+                    placeholder="chapter"
+                    value={chapter}
+                    onChangeText={text => {setChapter(text)}}
+                    style={styles.input}
+                    
+                    
+                />
+
+                <TextInput 
+                    placeholder="verse"
+                    value={verse}
+                    onChangeText={text => {setVerse(text)}}
+                    style={styles.input}
+                    
+                    
+                />    
+      
+      <TouchableOpacity onPress={fetchVerses}>
+        <Text>find</Text>
+        </TouchableOpacity>
+        <Text>{verses}</Text>
     </View>
   )
 }
