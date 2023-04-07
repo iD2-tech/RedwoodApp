f7fecd5641c693514b7a65d43cbcd5677bf9d340
DavyHyun <dhyun1124@gmail.com>
2023-04-07 15:18:24 -0700

    Front end completed

--- components/EachPost.js
+++ components/EachPost.js
@@ -1,9 +1,19 @@
 import { StyleSheet, Text, View, Animated, Dimensions } from 'react-native'
-import React, {useState} from 'react'
+import React, {useState, useCallback} from 'react'
 
 
 const EachPost = (props) => {
 
+    const [textShown, setTextShown] = useState(false); //To show ur remaining Text
+    const [lengthMore,setLengthMore] = useState(false); //to show the "Read more & Less Line"
+    const toggleNumberOfLines = () => { //To toggle the show text or hide it
+        setTextShown(!textShown);
+    }
+
+    const onTextLayout = useCallback(e =>{
+        setLengthMore(e.nativeEvent.lines.length >=4); //to check the text is more than 4 lines or not
+        // console.log(e.nativeEvent);
+    },[]);
     
 
   return (
@@ -15,7 +25,17 @@ const EachPost = (props) => {
       <Text style={styles.title}>{props.title}</Text>
       <Text style={styles.verseText}>" {props.verseText} "</Text>
       <Text style={styles.verse}>{props.verse}</Text>
-      <Text style={styles.text}>{props.text}</Text>
+      <Text style={styles.text}
+        onTextLayout={onTextLayout}
+        numberOfLines={textShown ? undefined : 4}
+      >{props.text}</Text>
+      {
+                  lengthMore ? <Text
+                  onPress={toggleNumberOfLines}
+                  style={{fontFamily: 'Lato-Regular', color: '#505050', marginTop: '2%', fontSize: 10}}
+                 >{textShown ? 'Read less...' : 'Read more...'}</Text>
+                  :null
+              }
     </View>
   )
 }
@@ -25,7 +45,7 @@ export default EachPost
 const styles = StyleSheet.create({
     container: {
         width: '80%',
-        marginBottom: '20%',
+        marginBottom: '15%',
 
         // alignItems: 'center' 
     },
