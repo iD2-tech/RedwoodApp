275634aed428d182a1d442de737a32f9828f830b
cokim5 <54604408+cokim5@users.noreply.github.com>
2023-04-15 15:21:51 -0700

    realm sync test across devices

--- screens/In/Profile.js
+++ screens/In/Profile.js
@@ -11,101 +11,7 @@ import Post from '../../database/Models/Post';
 
 
 
-const { width, height } = Dimensions.get('window')
-
-
-const DATA = [
-  {
-    id: '1',
-    user: 'David Hyun',
-    date: '04/01/2023',
-    title: 'Calling from God',
-    verseText: 'Create in me a pure heart, O God, and renew a steadfast spirit within me. Do not cast me from your presence or take your Holy Spirit from me. Restore to me the joy of your salvation and grant me a willing spirit, to sustain me',
-    verse: 'Psalm 51:10-12',
-    text: 'How can I expect to win my battles entering the battlefield with zero preparation? No armor, no weapon, vulnerable. If I went to war in real life like that Id die in seconds! As it is with my spiritual warfare.'
-      + 'I need to spend time with God, dwell and read his word every given opportunity I have, and put on the armor of God if I want to win my battles.',
-  },
-  {
-    id: '2',
-    user: 'Hanara Nam',
-    date: '04/02/2023',
-    title: 'Eye on Jesus',
-    verseText: 'Then Peter got down out of the boat, walked on the water and came toward Jesus. But when he saw the wind, he was afraid and, beginning to sink, cried out, "Lord, save me!',
-    verse: 'Matthew 14:29-30',
-    text: 'I realize the importance of starting the day off with God and spending time with him on the daily and keeping my eyes on him. When I begin lacking in my efforts to spend time with God, I find myself falling, living life with worldly vision, indulging in worldly pleasures. Live everyday with my eyes fixed on Jesus, because quite frankly, the world we live in is a storm in itself and once my eyes begin to be distracted with the situations and everything around me, I sink. Following Jesus requires complete focus on him.'
-  },
-  {
-    id: '3',
-    user: 'Collin Kim',
-    date: '04/03/2023',
-    title: 'Rejoice in the Lord!',
-    verseText: 'Rejoice in the Lord always. I will say it again: Rejoice!',
-    verse: 'Philippians 4:4',
-    text: 'How can I expect to win my battles entering the battlefield with zero preparation? No armor, no weapon, vulnerable. If I went to war in real life like that Id die in seconds! As it is with my spiritual warfare.' +
-      'I need to spend time with God, dwell and read his word every given opportunity I have, and put on the armor of God if I want to win my battles.'
-  },
-  {
-    id: '4',
-    user: 'David Hyun',
-    date: '04/04/2023',
-    title: 'Calling from God',
-    verseText: 'Create in me a pure heart, O God, and renew a steadfast spirit within me. Do not cast me from your presence or take your Holy Spirit from me. Restore to me the joy of your salvation and grant me a willing spirit, to sustain me',
-    verse: 'Psalm 51:10-12',
-    text: 'How can I expect to win my battles entering the battlefield with zero preparation? No armor, no weapon, vulnerable. If I went to war in real life like that Id die in seconds! As it is with my spiritual warfare.'
-      + 'I need to spend time with God, dwell and read his word every given opportunity I have, and put on the armor of God if I want to win my battles.',
-  },
-  {
-    id: '5',
-    user: 'Hanara Nam',
-    date: '04/05/2023',
-    title: 'Eye on Jesus',
-    verseText: 'Then Peter got down out of the boat, walked on the water and came toward Jesus. But when he saw the wind, he was afraid and, beginning to sink, cried out, "Lord, save me!',
-    verse: 'Matthew 14:29-30',
-    text: 'I realize the importance of starting the day off with God and spending time with him on the daily and keeping my eyes on him. When I begin lacking in my efforts to spend time with God, I find myself falling, living life with worldly vision, indulging in worldly pleasures. Live everyday with my eyes fixed on Jesus, because quite frankly, the world we live in is a storm in itself and once my eyes begin to be distracted with the situations and everything around me, I sink. Following Jesus requires complete focus on him.'
-  },
-  {
-    id: '6',
-    user: 'Collin Kim',
-    date: '04/06/2023',
-    title: 'Rejoice in the Lord!',
-    verseText: 'Rejoice in the Lord always. I will say it again: Rejoice!',
-    verse: 'Philippians 4:4',
-    text: 'How can I expect to win my battles entering the battlefield with zero preparation? No armor, no weapon, vulnerable. If I went to war in real life like that Id die in seconds! As it is with my spiritual warfare.' +
-      'I need to spend time with God, dwell and read his word every given opportunity I have, and put on the armor of God if I want to win my battles.'
-  },
-  {
-    id: '7',
-    user: 'David Hyun',
-    date: '04/01/2023',
-    title: 'Calling from God',
-    verseText: 'Create in me a pure heart, O God, and renew a steadfast spirit within me. Do not cast me from your presence or take your Holy Spirit from me. Restore to me the joy of your salvation and grant me a willing spirit, to sustain me',
-    verse: 'Psalm 51:10-12',
-    text: 'How can I expect to win my battles entering the battlefield with zero preparation? No armor, no weapon, vulnerable. If I went to war in real life like that Id die in seconds! As it is with my spiritual warfare.'
-      + 'I need to spend time with God, dwell and read his word every given opportunity I have, and put on the armor of God if I want to win my battles.',
-  },
-  {
-    id: '8',
-    user: 'Hanara Nam',
-    date: '04/01/2023',
-    title: 'Eye on Jesus',
-    verseText: 'Then Peter got down out of the boat, walked on the water and came toward Jesus. But when he saw the wind, he was afraid and, beginning to sink, cried out, "Lord, save me!',
-    verse: 'Matthew 14:29-30',
-    text: 'I realize the importance of starting the day off with God and spending time with him on the daily and keeping my eyes on him. When I begin lacking in my efforts to spend time with God, I find myself falling, living life with worldly vision, indulging in worldly pleasures. Live everyday with my eyes fixed on Jesus, because quite frankly, the world we live in is a storm in itself and once my eyes begin to be distracted with the situations and everything around me, I sink. Following Jesus requires complete focus on him.'
-  },
-  {
-    id: '9',
-    user: 'Collin Kim',
-    date: '04/01/2023',
-    title: 'Rejoice in the Lord!',
-    verseText: 'Rejoice in the Lord always. I will say it again: Rejoice!',
-    verse: 'Philippians 4:4',
-    text: 'How can I expect to win my battles entering the battlefield with zero preparation? No armor, no weapon, vulnerable. If I went to war in real life like that Id die in seconds! As it is with my spiritual warfare.' +
-      'I need to spend time with God, dwell and read his word every given opportunity I have, and put on the armor of God if I want to win my battles.'
-  },
-
-
-]
-
+const { width, height } = Dimensions.get('window');
 
 const Profile = ({ route }) => {
   var userId = firebase.auth().currentUser.email;
@@ -120,12 +26,19 @@ const Profile = ({ route }) => {
   const user = useUser();
   console.log(user.id);
   const realm = useRealm();
+
+  /*
   const allSubscriptions = realm.subscriptions;
 
   const posts = useQuery(Post);
   const usersPosts = posts.filtered(
     `user == "${user.id}"`
   )
+  */
+
+  // const usersPosts = useQuery('Post').filtered(`user == "${user.id}"`);
+
+  const usersPosts = realm.objects('Post').filtered(`user == "${user.id}"`);
 
 
   useEffect(() => {
@@ -133,10 +46,12 @@ const Profile = ({ route }) => {
       const usersPostsSubQuery = realm
         .objects('Post')
         .filtered(`user == "${user.id}"`);
-
       mutableSubs.add(usersPostsSubQuery, { name: 'usersPosts' });
+
+      // mutableSubs.add(usersPosts);
+      console.log(usersPostsSubQuery);
     }).then(() => {
-      console.log(allSubscriptions.length);
+      console.log(usersPosts);
     })
   });
 
@@ -175,7 +90,7 @@ const Profile = ({ route }) => {
 
     <View style={styles.listContainer}>
       <FlatList
-          data={allSubscriptions}
+          data={usersPosts}
           renderItem={({item}) => 
           <TouchableOpacity onPress={() => onItemPress(item)}>
             <EachJournal user={item.user} date={item.createdAt} title={item.title} verseText={item.bibleVerses} verse={item.book + " " + item.chapter + ":" + item.verse} text={item.text}/>
